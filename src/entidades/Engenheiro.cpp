1cd45c2ff342f50ea3ceb1e2ba028a1e630924d6
joaocarvalho74 <102688729+joaocarvalho74@users.noreply.github.com>
2023-06-28 21:48:08 -0300

    Create Engenheiro.cpp

---
+++ src/entidades/Engenheiro.cpp
@@ -0,0 +1,14 @@
+#include "Engenheiro.hpp"
+
+/**
+ * @brief Construtor da classe Engenheiro
+ * @param _nome Nome do engenheiro instanciado
+ * @param _identificacao Número de identificação
+ *do engenheiro instanciado
+ * @param _idade Idade do engenheiro instanciado
+ * @param _salario Salário do engenheiro instanciado
+ */
+Engenheiro::Engenheiro(std::string _nome, int _identificacao, int _idade, double _salario)
+              : Funcionario(_nome, _identificacao, _idade, _salario){};
+
+/**
