5165838266b8c942feabb6ec191ec0fa3a3e6219
vinicius-souzap <103080317+vinicius-souzap@users.noreply.github.com>
2023-07-06 09:21:52 -0300

    Update Contador.hpp

--- include/Contador.hpp
+++ include/Contador.hpp
@@ -13,15 +13,16 @@ class OrdemServico;
  * confirmar o pagamento dos contratos e gerar relatórios relacionados ao
  * financeiro da empresa
  */
-
 class Contador : public Funcionario
 {
-    private:
+   
+private:
         std::vector<OrdemServico*> listaOrdens;
         double receitaBruta;                       //trata-se do total de pagamentos recebidos;
         double receitaPendente;                    //trata-se do total pendente das OS cujos clientes
                                                    //ainda não realizaram o pagamento;
-    public:
+   
+public:
         Contador(std::string _nome, int _identificacao, int _idade, double _salario, std::vector<OrdemServico*> _listaOrdens);
         virtual ~Contador();
         void setPagamento(int _numOrdem, double _valor, std::string _dataPagamento);
